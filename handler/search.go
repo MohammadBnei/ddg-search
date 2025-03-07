1848ef784c9017925a127b74e0f5f920e7d0eb63
MohammadBnei <mohammadamine.banaei@pm.me>
2025-03-07 11:33:00 +0100

    fix: removed old handler

--- handler/search.go
+++ handler/search.go
@@ -3,6 +3,7 @@ package handler
 import (
 	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
 	"strconv"
 
@@ -98,4 +99,14 @@ func (h *SearchHandler) Handle(w http.ResponseWriter, r *http.Request) {
 		writeError(w, fmt.Errorf("failed to encode response: %w", err), http.StatusInternalServerError)
 		return
 	}
+}
+
+func writeError(w http.ResponseWriter, err error, code int) {
+	log.Println(err)
+	w.WriteHeader(code)
+	json.NewEncoder(w).Encode(struct {
+		Error string `json:"error"`
+	}{
+		Error: err.Error(),
+	})
 }
